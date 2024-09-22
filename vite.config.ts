e9c9e7c5dec7fb6f361b6ec092ea09b1e9f92706
Udezi Oghenero <udezioghenero@mail.com>
2024-09-22 19:31:27 +0200

    add for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  // If your app is deployed at the root domain, '/' is fine.
+  // If it's under a subpath, adjust accordingly.
+  base: '/',
+});
